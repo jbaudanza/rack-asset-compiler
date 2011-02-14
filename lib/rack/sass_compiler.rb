ab483cf90006a9048ba6b67ee3e9b8fcd044565a
Jonathan Baudanza <jon@jonb.org>
2011-02-14 00:45:38 -0800

    Added Rack::SassCompiler

---
+++ lib/rack/sass_compiler.rb
@@ -0,0 +1,31 @@
+require 'rack/asset_compiler'
+require 'sass'
+
+module Rack
+  class SassCompiler < AssetCompiler
+    attr_accessor :syntax
+
+    def initialize(app, options={})
+      options
+
+      options = {
+        :url => '/stylesheets',
+        :content_type => 'text/css',
+        :syntax => :scss
+      }.merge(options)
+
+      @syntax = options[:syntax]
+      options[:source_extension] ||= syntax.to_s
+      super
+    end
+
+    def compile(source_file)
+      sass_options = {
+        :syntax => syntax,
+        :cache => false,
+        :load_paths => [source_dir]
+      }
+      Sass::Engine.new(::File.read(source_file), sass_options).render
+    end
+  end
+end
---
+++ spec/sass_compiler_spec.rb
@@ -0,0 +1,41 @@
+require 'rack/sass_compiler'
+require 'rack/lobster'
+require "rack/test"
+
+include Rack::Test::Methods
+
+describe "SassCompiler" do
+  before do
+    @source_dir = File.join(File.dirname(__FILE__), 'fixtures/sass')
+
+    @options = {
+      :source_dir => @source_dir
+    }
+  end
+
+  def app
+    options = @options
+    Rack::Builder.new {
+      use Rack::Lint
+      use Rack::SassCompiler, options
+      run Rack::Lobster.new
+    }
+  end
+
+  it "should compile scss" do
+    get '/stylesheets/style.css'
+    last_response.body.should ==
+      Sass::Engine.new(File.read("#{@source_dir}/style.scss"), :syntax => :scss).render
+    last_response.status.should == 200
+    last_response.content_type.should == 'text/css'
+  end
+
+  it "should compile sass" do
+    @options[:syntax] = :sass
+    get '/stylesheets/style.css'
+    last_response.body.should ==
+      Sass::Engine.new(File.read("#{@source_dir}/style.sass"), :syntax => :sass).render
+    last_response.status.should == 200
+    last_response.content_type.should == 'text/css'
+  end
+end
