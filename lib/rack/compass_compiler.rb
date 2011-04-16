5ac88bca2907d848bc4a15bfd84135ba7f239921
Derek Prior <derekprior@gmail.com>
2011-04-15 21:34:29 -0400

    compass support refactoring

--- lib/rack/compass_compiler.rb
+++ lib/rack/compass_compiler.rb
@@ -1,35 +1,16 @@
-require 'rack/asset_compiler'
+require 'rack/sass_compiler'
 require 'compass'
 
 module Rack
-  class CompassCompiler < AssetCompiler
-    attr_accessor :syntax
+  class CompassCompiler < SassCompiler
 
-    def initialize(app, options={})
-      options
-
-      options = {
-        :url => '/stylesheets',
-        :content_type => 'text/css',
-        :syntax => :scss
-      }.merge(options)
-
-      @syntax = options[:syntax]
-      options[:source_extension] ||= syntax.to_s
-      super
-    end
-
-    def compile(source_file)
-      compass_paths = []
+    def get_load_paths(src_dir)
+      paths = [src_dir]
       Compass::Frameworks::ALL.each do |framework|
-        compass_paths << framework.stylesheets_directory if ::File.exists?(framework.stylesheets_directory)
+        paths << framework.stylesheets_directory if ::File.exists?(framework.stylesheets_directory)
       end
-      sass_options = {
-        :syntax => syntax,
-        :cache => false,
-        :load_paths => [source_dir] + compass_paths
-      }
-      Sass::Engine.new(::File.read(source_file), sass_options).render
+      paths
     end
+
   end
 end
--- lib/rack/sass_compiler.rb
+++ lib/rack/sass_compiler.rb
@@ -19,11 +19,15 @@ module Rack
       super
     end
 
+    def get_load_paths(src_dir)
+      [src_dir]
+    end
+
     def compile(source_file)
       sass_options = {
         :syntax => syntax,
         :cache => false,
-        :load_paths => [source_dir]
+        :load_paths => get_load_paths(source_dir)
       }
       Sass::Engine.new(::File.read(source_file), sass_options).render
     end
